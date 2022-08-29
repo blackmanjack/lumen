33ae69004d6d04e18933ff6241a51d56a90b178a
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-08-29 16:40:10 +0700

    feat: add seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        User::create([
+            'username' => 'admin',
+            'email' => 'lumenpostgresql@gmail.com',
+            'password' => Hash::make('adminLumen22'),
+            'is_admin' => true,
+            'status' => true,
+            'token' => base64_encode(Str::random(32))   
+        ]);
     }
 }
