dbca916cfa54308d22316958f83b417f770d038b
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-08-31 18:00:46 +0700

    feat: add new validation and auth nodeController

--- app/Http/Controllers/NodeController.php
+++ app/Http/Controllers/NodeController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Hardware;
 use App\Models\Sensor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NodeController extends Controller
 {
@@ -21,26 +22,37 @@ class NodeController extends Controller
     }
 
     public function create(Request $request)
-    {
-        $data = new Node();
-        $data->hardware_id = $request->hardware_id;
-        $data->user_id = $request->user_id;
-        $data->name = $request->name;
-        $data->location = $request->location;
-        $save = $data->save();
+    {   
+        $this->validate($request, [
+            'hardware_id' => 'required',
+            'name' => 'required',
+            'location' => 'required'
+        ]);
 
-        if($save){
-            $message = "Success add new node";
-            return response()->json($message, 201);
-        }else{
-            $message = "Parameter is Invalid";
-            return response()->json($message, 404);
-        }
+        $node = new Node();
+        $node->hardware_id = $request->hardware_id;
+        $node->user_id = Auth::id();
+        $node->name = $request->name;
+        $node->location = $request->location;
+        $node = $node->save();
+        
+        $message = "Success add new node";
+        return response()->json($message, 201);
+
+        // if($node){
+        //     $message = "Success add new node";
+        //     return response()->json($message, 201);
+        // }else{
+        //     $message = "Parameter is Invalid";
+        //     return response()->json($message, 404);
+        // }
     }
 
     public function showAll()
     {
-        $data = Node::all();
+        $userid = Auth::id();
+        $data = Node::where('user_id', $userid)->get();
+        // dd($data);
         $response=
             // 'data'=> $data
             $data
@@ -51,40 +63,56 @@ class NodeController extends Controller
     public function showDetailData($id)
     {
         //query user and hardware
-        $data = Node::where('id', $id)->with('User','Hardware', 'Sensor.Channel')->first();
-        
+        $userid = Auth::id();
+
+        $data = Node::where('user_id', $userid)
+        ->where('id', $id)
+        ->with('User','Hardware', 'Sensor.Channel')
+        ->first();
+
         if($data){
             return response()->json($data, 200);
         }
         else{
             $message = "Not found";
             return response()->json($message, 404);
-            }
+        }
     }
 
     public function update(Request $request, $id)
     {
-        $data = Node::find($id);
-        // dd($request->all());
-        // return response($request);
+        $userid = Auth::id();
+
+        $this->validate($request, [
+            'name' => 'required',
+            'location' => 'required'
+        ]);
+
+        $data = Node::where('user_id', $userid)->find($id);
+
         $update = $data->update([
             'name'=> $request->name,
             'location'=> $request->location,
         ]);
 
-         if($update){
-            $message = "Success edit node";
-            return response()->json($message, 200);
-        }else{
-            $message = "Empty Request Body";
-            return response()->json($message, 400);
-        }
-        return response($response);
+        $message = "Success edit node";
+        return response()->json($message, 200);
+
+        // if($update){
+        //     $message = "Success edit node";
+        //     return response()->json($message, 200);
+        // }else{
+        //     $message = "Empty Request Body";
+        //     return response()->json($message, 400);
+        // }
+        // return response($response);
     }
 
     public function delete($id)
     {
-        $data = Node::find($id);
+        $userid = Auth::id();
+        $data = Node::where('user_id', $userid)->find($id);
+        
         $delete = $data->delete();
 
         if($delete){
