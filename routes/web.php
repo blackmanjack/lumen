e33cf5936e8f5978fe5116a2bb9241b44ead7c95
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-08-31 15:23:17 +0700

    feat: add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -23,31 +23,37 @@ $router->get('/key', function() {
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
 //user
 $router->post('/user/signup', 'UserController@create');
 $router->get('/user/activation', 'UserController@activate');
 $router->post('/user/login', 'UserController@login');
 $router->post('/user/forget-password', 'UserController@resetpasswd');
-$router->get('/user/{id}', 'UserController@showAllDataUser');
-$router->put('/user/{id}', 'UserController@update');
-$router->delete('/user/{id}', 'UserController@delete');
-//node
-$router->post('/node', 'NodeController@create');
-$router->get('/node', 'NodeController@showAll');
-$router->get('/node/{id}', 'NodeController@showDetailData');
-$router->put('/node/{id}', 'NodeController@update');
-$router->delete('/node/{id}', 'NodeController@delete');
-//hardware
-$router->post('/hardware', 'HardwareController@create');
-$router->get('/hardware', 'HardwareController@showAll');
-$router->get('/hardware/{id}', 'HardwareController@showDetailData');
-$router->put('/hardware/{id}', 'HardwareController@update');
-$router->delete('/hardware/{id}', 'HardwareController@delete');
-//sensor
-$router->post('/sensor', 'SensorController@create');
-$router->get('/sensor', 'SensorController@showAll');
-$router->get('/sensor/{id}', 'SensorController@showDetailData');
-$router->put('/sensor/{id}', 'SensorController@update');
-$router->delete('/sensor/{id}', 'SensorController@delete');
-//channel
-$router->post('/channel', 'ChannelController@create');
+
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('/user/{id}', 'UserController@showAllDataUser');
+
+    $router->put('/user/{id}', 'UserController@update');
+    $router->delete('/user/{id}', 'UserController@delete');
+    //node
+    $router->post('/node', 'NodeController@create');
+    $router->get('/node', 'NodeController@showAll');
+    $router->get('/node/{id}', 'NodeController@showDetailData');
+    $router->put('/node/{id}', 'NodeController@update');
+    $router->delete('/node/{id}', 'NodeController@delete');
+    //hardware
+    $router->post('/hardware', 'HardwareController@create');
+    $router->get('/hardware', 'HardwareController@showAll');
+    $router->get('/hardware/{id}', 'HardwareController@showDetailData');
+    $router->put('/hardware/{id}', 'HardwareController@update');
+    $router->delete('/hardware/{id}', 'HardwareController@delete');
+    //sensor
+    $router->post('/sensor', 'SensorController@create');
+    $router->get('/sensor', 'SensorController@showAll');
+    $router->get('/sensor/{id}', 'SensorController@showDetailData');
+    $router->put('/sensor/{id}', 'SensorController@update');
+    $router->delete('/sensor/{id}', 'SensorController@delete');
+    //channel
+    $router->post('/channel', 'ChannelController@create');
+});
+
