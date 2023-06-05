9e21963e98790f9ae9e8912c6ef38b94d9fa10b2
blackmanjack <faiz_m17@apps.ipb.ac.id>
2023-06-05 14:32:51 +0700

    feat(db): add init db

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -1,11 +1,10 @@
 create table user_person(
     id_user serial primary key,
-    email varchar (255) not null,
-    username varchar (255) not null,
+    username varchar (255) not null UNIQUE,
+    email varchar (255) not null UNIQUE,                    
     password varchar (255) not null,
-    status boolean default false not null,
-    token varchar (255) not null,
-    isadmin boolean default false not null
+    status BOOLEAN DEFAULT false,
+    isadmin BOOLEAN DEFAULT false                    
 );
 create table hardware(
     id_hardware serial primary key,
@@ -15,25 +14,45 @@ create table hardware(
 );
 create table node(
     id_node serial primary key,
+    id_user integer not null,                    
+    id_hardware_node integer not null, 
+    id_hardware_sensor integer[10], 
     name varchar (255) not null, 
     location varchar (255) not null, 
-    id_hardware integer, 
-    id_user integer not null, 
-    foreign key (id_hardware) references hardware (id_hardware) on update cascade,
-    foreign key (id_user) references user_person (id_user) on update cascade
+    field_sensor text[10] not null,
+    is_public BOOLEAN default false,
+    foreign key (id_hardware_node) references hardware (id_hardware) on update cascade on delete cascade,
+    foreign key (id_user) references user_person (id_user) on update cascade on delete cascade
 );
-create table sensor(
-    id_sensor serial primary key, 
-    name varchar (255) not null, 
-    unit varchar (255) not null,
-    id_hardware integer, 
-    id_node integer not null, 
-    foreign key (id_hardware) references hardware (id_hardware) on update cascade on delete cascade,   
-    foreign key (id_node) references node (id_node) on update cascade on delete cascade
-);
-create table channel(
-    time timestamp, 
-    value float not null, 
-    id_sensor integer not null, 
-    foreign key (id_sensor) references sensor (id_sensor) on update cascade on delete cascade
+ALTER TABLE node
+    ALTER COLUMN field_sensor SET DEFAULT '{}'::text[];
+
+CREATE OR REPLACE FUNCTION set_default_field_sensor()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.field_sensor[1] := '';
+    NEW.field_sensor[2] := '';
+    NEW.field_sensor[3] := '';
+    NEW.field_sensor[4] := '';
+    NEW.field_sensor[5] := '';
+    NEW.field_sensor[6] := '';
+    NEW.field_sensor[7] := '';
+    NEW.field_sensor[8] := '';
+    NEW.field_sensor[9] := '';
+    NEW.field_sensor[10] := '';
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_default_field_sensor_trigger
+    BEFORE INSERT OR UPDATE ON node
+    FOR EACH ROW
+    WHEN (pg_trigger_depth() = 0)
+    EXECUTE FUNCTION set_default_field_sensor();
+    
+create table feed(
+    id_node integer not null,
+    time timestamp NOT NULL,
+    value float[10] NOT NULL,
+    foreign key (id_node) references node (id_node) on update cascade on delete cascade                    
 );
