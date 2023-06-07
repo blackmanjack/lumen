51214775b25d8810fce8c74fb6e0efed61b8e1d7
blackmanjack <faiz_m17@apps.ipb.ac.id>
2023-06-07 10:00:24 +0700

    feat(node): add hardware relation and get datatype

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -16,6 +16,67 @@ class Node extends Model
     public $timestamps = false;
     protected $primaryKey = 'id_node';
 
+    /**
+     * Get the field_sensor attribute as an array.
+     *
+     * @param  string  $value
+     * @return array|null
+     */
+    public function getFieldSensorAttribute($value)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $fieldSensorArray = explode(',', str_replace(['{', '}'], '', $value));
+
+        return $fieldSensorArray;
+    }
+
+    /**
+     * Get the id_hardware_sensor attribute as an array.
+     *
+     * @param  string  $value
+     * @return array|null
+     */
+    public function getIdHardwareSensorAttribute($value)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $fieldIdHardwareSensor = explode(',', str_replace(['{', '}'], '', $value));
+        // Loop through each element in the array
+        foreach ($fieldIdHardwareSensor as $key => $element) {
+            // if ($element === "NULL") {
+            //     $fieldIdHardwareSensor[$key] = null;
+            // }
+            if (is_numeric($element)) {
+                // Convert the element to the appropriate numeric type
+                if (strpos($element, '.') !== false) {
+                    $fieldIdHardwareSensor[$key] = floatval($element);
+                } else {
+                    $fieldIdHardwareSensor[$key] = intval($element);
+                }
+            }
+            // // Check if the element is "NULL"
+            // if ($element === "NULL") {
+            //     $fieldIdHardwareSensor[$key] = "NULL";
+            // } else {
+            //     // Check if the element is numeric
+            //     if (is_numeric($element)) {
+            //         // Convert the element to the appropriate numeric type
+            //         if (strpos($element, '.') !== false) {
+            //             $fieldIdHardwareSensor[$key] = floatval($element);
+            //         } else {
+            //             $fieldIdHardwareSensor[$key] = intval($element);
+            //         }
+            //     }
+            // }
+        }
+        return $fieldIdHardwareSensor;
+    }
+
 	public function user()
     {
         return $this->belongsTo(User::class, 'id_user');
@@ -26,8 +87,8 @@ class Node extends Model
         return $this->belongsTo(Hardware::class, 'id_hardware');
     }
 
-	public function sensor()
-    {
-        return $this->hasMany(Sensor::class, 'id_sensor');
-    }
+	// public function sensor()
+    // {
+    //     return $this->hasMany(Sensor::class, 'id_sensor');
+    // }
 }
