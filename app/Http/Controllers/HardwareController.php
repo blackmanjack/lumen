9816eb4613e6e2563bc761579b9898905388afee
blackmanjack <faiz_m17@apps.ipb.ac.id>
2023-07-13 17:23:15 +0700

    refactor(hardware): refactor and add err boundary

--- app/Http/Controllers/HardwareController.php
+++ app/Http/Controllers/HardwareController.php
@@ -23,145 +23,129 @@ class HardwareController extends Controller
 
     public function create(Request $request)
     {
-        //check input field is empty
-        $this->validate($request, [
-            'name' => 'required',
-            'type' => 'required',
-            'description' => 'required'
-        ]);
+        try{
+            //check input field is empty
+            $this->validate($request, [
+                'name' => 'required|string|max:256',
+                'type' => 'required|string|max:256',
+                'description' => 'required|string|max:256'
+            ]);
 
-        $userId = Auth::id();
-        // $isAdmin = User::where('id_user', $userId)->pluck('isadmin')->first();
-
-        // if(!$isAdmin){
-        //     $message = "You're not admin";
-        //     return response()->json($message, 401);
-        // }
-        
-        $data = new Hardware();
-        $data->name = $request->name;
-        $data->type = strtolower($request->type);
-        $data->description = $request->description;
-        
-        if($data->type === 'single-board computer' || $data->type === 'microcontroller unit' || $data->type === 'sensor'){
-            $data->save();
-            $message = "Success add new hardware";
-            return response()->json($message, 201);
-        }else{
-            $message = "Type must Single-Board Computer, Microcontroller Unit, or Sensor";
-            return response()->json($message, 400);
+            $userId = Auth::id();
+            
+            $data = new Hardware();
+            $data->name = $request->name;
+            $data->type = strtolower($request->type);
+            $data->description = $request->description;
+            
+            if($data->type === 'single-board computer' || $data->type === 'microcontroller unit' || $data->type === 'sensor'){
+                $data->save();
+                $message = "Success add new hardware";
+                return response()->json($message, 201);
+            }else{
+                $message = "Type must Single-Board Computer, Microcontroller Unit, or Sensor";
+                return response()->json($message, 400);
+            }
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Server error'], 500);
         }
     }
 
     public function showAll()
     {
-        // $data = Hardware::select('hardware.*')
-        //         ->join('nodes', 'hardware.id', '=', 'nodes.id_hardware')
-        //         ->where('nodes.id_user', Auth::id())   
-        //         ->with('Sensor', 'Node')
-        //         ->get();
-
-        $data = Hardware::select('hardware.*')  
+        try{
+            $data = Hardware::select('hardware.*')  
                 ->with('Sensor','Node')
                 ->get();
-
-        // $userID = $data->toArray()['node'][0]['id_user'];
-        // $data = Hardware::
-        //         with('Node', 'Sensor')
-        //         ->first();
-        return response($data);
+            return response($data);
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Server error'], 500);
+        }
     }
 
     public function showDetailData($id)
     {
-        $id_user = Auth::id();
-        $key = ['name' => 'Abigail', 
-                'state' => 'CA'];
-        $tes = json_encode($key);
-
-        // $findHardware = Node::where('id_user', $id_user)->pluck('id_hardware')->toArray();
+        try{
+            $id_user = Auth::id();
     
-        $data = Hardware::where('id_hardware', $id)->with('Node', 'Sensor')->first();
+            $data = Hardware::where('id_hardware', $id)->with('Node', 'Sensor')->first();
 
-        if($data){
-            return response()->json($data, 200);
-        }
-
-        // $node = $data->toArray()['node'];
-        // if($data && $node !== []){
-        //     //cek node
-        //     $userID = $data->toArray()['node'][0]['id_user'];
-        //     if($userID === Auth::id()){
-        //         return response()->json($data, 200);
-        //     }else{
-        //         $message = 'You can\'t see another user\'s hardware';
-        //         return response()->json($message, 403);
-        //     }
-        // }
-        else{
-            $message = "Not found";
-            return response()->json($message, 404);
+            if($data){
+                return response()->json($data, 200);
+            }else{
+                $message = "Not found";
+                return response()->json($message, 404);
+            }
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Invalid Input'], 400);
         }
     }
 
     public function update(Request $request, $id)
     {
-        $data = Hardware::where('id_hardware', $id)->first();
-        if($data == null){
-            $message = "Hardware Not Found";
-            return response()->json($message, 404);
-        }
-        //only accept headers application/x-www-form-urlencoded
-        $contentType = $request->headers->get('Content-Type');
-        $split = explode(';', $contentType)[0];
-        if($split !== "application/x-www-form-urlencoded"){
-            $message = "Supported format: application/x-www-form-urlencoded";
-            return response()->json($message, 415);
-        }
-
-        //check input field is empty
-        $this->validate($request, [
-            'name' => 'required',
-            'type' => 'required',
-            'description' => 'required'
-        ]);
-
-        if($request->type === 'single-board computer' || $request->type === 'microcontroller unit' || $request->type === 'sensor'){
-            $update = $data->update([
-                'name'=> $request->name,
-                'type'=> strtolower($request->type),
-                'description'=> $request->description,
+        try{
+            $data = Hardware::where('id_hardware', $id)->first();
+            if($data == null){
+                $message = "Hardware Not Found";
+                return response()->json($message, 404);
+            }
+            //only accept headers application/x-www-form-urlencoded
+            $contentType = $request->headers->get('Content-Type');
+            $split = explode(';', $contentType)[0];
+            if($split !== "application/x-www-form-urlencoded"){
+                $message = "Supported format: application/x-www-form-urlencoded";
+                return response()->json($message, 415);
+            }
+    
+            //check input field is empty
+            $this->validate($request, [
+                'name' => 'required|string|max:256',
+                'type' => 'required|string|max:256',
+                'description' => 'required|string|max:256'
             ]);
-            $message = "Success edit Hardware";
-            return response()->json($message, 200);
-        }else{
-            $message = "Type must Single-Board Computer, Microcontroller Unit, or Sensor";
-            return response()->json($message, 400);
+    
+            if($request->type === 'single-board computer' || $request->type === 'microcontroller unit' || $request->type === 'sensor'){
+                $update = $data->update([
+                    'name'=> $request->name,
+                    'type'=> strtolower($request->type),
+                    'description'=> $request->description,
+                ]);
+                $message = "Success edit Hardware";
+                return response()->json($message, 200);
+            }else{
+                $message = "Type must Single-Board Computer, Microcontroller Unit, or Sensor";
+                return response()->json($message, 400);
+            }
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Invalid Input'], 400);
         }
     }
 
     public function delete($id)
     {
-        $data = Hardware::where('id_hardware', $id)
-        ->with('Node', 'Sensor')
-        ->first();
+        try{
+            $data = Hardware::where('id_hardware', $id)
+            ->with('Node', 'Sensor')
+            ->first();
 
-        //if data found in db
-        if($data){
-            //if hardware not found in node's table and sensor's table, delete hardware
-            if($data->toArray()['node'] === [] && $data->toArray()['sensor'] === []){
-                $data = Hardware::find($id);
-                $data->delete();
-                $message = 'Delete hardware, id:'.$id;
-                return response()->json($message, 200);
+            //if data found in db
+            if($data){
+                //if hardware not found in node's table and sensor's table, delete hardware
+                if($data->toArray()['node'] === [] && $data->toArray()['sensor'] === []){
+                    $data = Hardware::find($id);
+                    $data->delete();
+                    $message = 'Delete hardware, id:'.$id;
+                    return response()->json($message, 200);
+                }else{
+                    $message = 'Can\'t delete, hardware is still used';
+                    return response()->json($message, 400);
+                }
             }else{
-                $message = 'Can\'t delete, hardware is still used';
-                return response()->json($message, 400);
+                $message = 'Id hardware not found';
+                return response()->json($message, 404);
             }
-        }else{
-            $message = 'Id hardware not found';
-            return response()->json($message, 404);
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Invalid Input'], 400);
         }
-        
     }
 }
