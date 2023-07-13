4f5f6a6c915d0c843b8ba14df4eaa1d8e8399a9c
blackmanjack <faiz_m17@apps.ipb.ac.id>
2023-07-13 17:23:48 +0700

    refactor(sensor): refactor and add err boundary

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -22,146 +22,168 @@ class SensorController extends Controller
 
     public function create(Request $request)
     {
-        $this->validate($request, [
-            'id_node' => 'required',
-            'name' => 'required',
-            'unit' => 'required'
-        ]);
-
-        $data = new Sensor();
-        $data->id_node = $request->id_node;
-        $data->name = $request->name;
-        $data->unit = $request->unit;
-
-        $id_user = Node::where('id_node', $request->id_node)->pluck('id_user')->first();
-        //checking id node
-        $findNode = Node::where('id_node', $request->id_node)->first();
-        if($findNode){
-            //check user's node
-            if($id_user !== Auth::id()){
-                $message = 'You can\'t use another user\'s node';
-                return response()->json($message, 403);
-            }
-            //checking hardware
-            if($request->id_hardware == null || $request->id_hardware == ""){
-
-                $save = $data->save();
-                $message = "Success add new sensor";
-                return response()->json($message, 201);
-            }else{
-                $data->id_hardware = $request->id_hardware; 
-                //check hardware id
-                $hardwareId = Hardware::where('id_hardware', $request->id_hardware)->first();
-                if($hardwareId){
-                    //checking type hardware
-                    $typeHardware = Hardware::where('id_hardware', $request->id_hardware)->pluck('type')->first();
-                    if($typeHardware == 'sensor'){
-                        $save = $data->save();
-                        $message = "Success add new sensor";
-                        return response()->json($message, 201);
-                    }else{
-                        $message = 'Hardware type not match, type should Sensor';
-                        return response()->json($message, 400);
-                    }
-                }else {
-                    $message = 'Hardware Not Found';
-                    return response()->json($message, 404);
+        try{
+            $this->validate($request, [
+                'id_node' => 'required|integer',
+                'name' => 'required|string|max:256',
+                'unit' => 'required|string|max:256'
+            ]);
+    
+            $data = new Sensor();
+            $data->id_node = $request->id_node;
+            $data->name = $request->name;
+            $data->unit = $request->unit;
+    
+            $id_user = Node::where('id_node', $request->id_node)->pluck('id_user')->first();
+            //checking id node
+            $findNode = Node::where('id_node', $request->id_node)->first();
+            if($findNode){
+                //check user's node
+                if($id_user !== Auth::id()){
+                    $message = 'You can\'t use another user\'s node';
+                    return response()->json($message, 403);
                 }
+                //checking hardware
+                if($request->id_hardware == null || $request->id_hardware == ""){
+    
+                    $save = $data->save();
+                    $message = "Success add new sensor";
+                    return response()->json($message, 201);
+                }else{
+                    $data->id_hardware = $request->id_hardware; 
+                    //check hardware id
+                    $hardwareId = Hardware::where('id_hardware', $request->id_hardware)->first();
+                    if($hardwareId){
+                        //checking type hardware
+                        $typeHardware = Hardware::where('id_hardware', $request->id_hardware)->pluck('type')->first();
+                        if($typeHardware == 'sensor'){
+                            $save = $data->save();
+                            $message = "Success add new sensor";
+                            return response()->json($message, 201);
+                        }else{
+                            $message = 'Hardware type not match, type should Sensor';
+                            return response()->json($message, 400);
+                        }
+                    }else {
+                        $message = 'Hardware Not Found';
+                        return response()->json($message, 404);
+                    }
+                }
+            }else{
+                $message = 'Id node not found';
+                return response()->json($message, 404);
             }
-        }else{
-            $message = 'Id node not found';
-            return response()->json($message, 404);
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Server error'], 500);
         }
+        
     }
 
     public function showAll()
     {
-        $data = Sensor::select('sensor.*')->leftJoin('node', 'node.id_node', '=', 'sensor.id_node')
-                ->where('node.id_user', Auth::id())
-                ->with('Node')    
-                ->get();
-        return response($data);
+        try{
+            $data = Sensor::whereHas('node', function ($query) {
+                    $query->where('id_user', Auth::id());
+                    })   
+                    ->get();
+            return response($data);
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Server error'], 500);
+        }
     }
 
     public function showDetailData($id)
     {
-        //query node, hardware, channel 
-        $findSensor = Sensor::where('id_sensor', $id)->first();
+        try{
+            //query node, hardware, channel 
+            $findSensor = Sensor::where('id_sensor', $id)->first();
+            if($findSensor){
+                $data = Sensor::with('channel')
+                ->whereHas('node', function ($query) {
+                    $query->where('id_user', Auth::id());
+                })
+                ->find($id);
 
-        //$data = Sensor::where('id_sensor', $id)->with('Node', 'Channel')->first();
-        //add You can\'t see another user\'s sensor
-        if($findSensor){
-            $data = Sensor::select('sensor.*')->where('id_sensor', $id)->leftJoin('node', 'node.id_node', '=', 'sensor.id_node')
-            ->where('node.id_user', Auth::id())
-            ->with('Node')    
-            ->get();
-            
-        return response()->json($data, 200);
+                //user validation
+                if (!$data) {
+                    $message = "You can't see another user's sensor";
+                    return response()->json($message, 403);
+                }
+                
+            return response()->json($data, 200);
 
-        }else {
-            $message = 'Id sensor not found';
-            return response()->json($message, 404);
+            }else {
+                $message = 'Id sensor not found';
+                return response()->json($message, 404);
+            }
+        }catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Invalid Input'], 400);
         }
+        
     }
 
     public function update(Request $request, $id)
     {
-        //only accept headers application/x-www-form-urlencoded
-        $contentType = $request->headers->get('Content-Type');
-        $split = explode(';', $contentType)[0];
-        if($split !== "application/x-www-form-urlencoded"){
-            $message = "Supported format: application/x-www-form-urlencoded";
-            return response()->json($message, 415);
-        }
-
-        //validation input
-        $this->validate($request, [
-            'name' => 'required',
-            'unit' => 'required'
-        ]);
-
-        $findSensor = Sensor::where('id_sensor', $id);
-
-        $data = Sensor::where('id_sensor', $id)->with('Node')->first();
-
-        $userID = $data->toArray()['node']['id_user'];
-        if($findSensor){
-            if($userID === Auth::id()){
-                $update = $data->update([
-                    'name'=> $request->name,
-                    'unit'=> $request->unit,
-                ]);
-
-                $message = "Success edit Sensor";
-                return response()->json($message, 200);
-            }else{
-                $message = 'You can\'t edit another user\'s sensor';
-                return response()->json($message, 403);
+        try {
+            //only accept headers application/x-www-form-urlencoded
+            $contentType = $request->headers->get('Content-Type');
+            $split = explode(';', $contentType)[0];
+            if($split !== "application/x-www-form-urlencoded"){
+                $message = "Supported format: application/x-www-form-urlencoded";
+                return response()->json($message, 415);
             }
-        }else{
-            $message = 'Id sensor not found';
-            return response()->json($message, 404);
+
+            //validation input
+            $this->validate($request, [
+                'name' => 'required|string|max:256',
+                'unit' => 'required|string|max:256'
+            ]);
+
+            $findSensor = Sensor::find($id);
+            $data = Sensor::where('id_sensor', $id)->with('Node')->first();
+            if($findSensor){
+                $userID = $data->toArray()['node']['id_user'];
+                if($userID === Auth::id()){
+                    $delete = $data->delete();
+    
+                    $message = "Success delete sensor data, id: $id";
+                    return response()->json($message, 200);
+                }else{
+                    $message = 'You can\'t delete another user\'s sensor';
+                    return response()->json($message, 403);
+                }
+            }else{
+                $message = 'Id sensor not found';
+                return response()->json($message, 404);
+            }
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Invalid Input'], 400);
         }
     }
 
     public function delete($id)
     {
-        $findSensor = Sensor::find($id);
-        $data = Sensor::where('id_sensor', $id)->with('Node')->first();
-        if($findSensor){
-            $userID = $data->toArray()['node']['id_user'];
-            if($userID === Auth::id()){
-                $delete = $data->delete();
-
-                $message = "Success delete sensor data, id: $id";
-                return response()->json($message, 200);
+        try{
+            $findSensor = Sensor::find($id);
+            $data = Sensor::where('id_sensor', $id)->with('Node')->first();
+            if($findSensor){
+                $userID = $data->toArray()['node']['id_user'];
+                if($userID === Auth::id()){
+                    $delete = $data->delete();
+    
+                    $message = "Success delete sensor data, id: $id";
+                    return response()->json($message, 200);
+                }else{
+                    $message = 'You can\'t delete another user\'s sensor';
+                    return response()->json($message, 403);
+                }
             }else{
-                $message = 'You can\'t delete another user\'s sensor';
-                return response()->json($message, 403);
+                $message = 'Id sensor not found';
+                return response()->json($message, 404);
             }
-        }else{
-            $message = 'Id sensor not found';
-            return response()->json($message, 404);
+        }
+        catch (\Illuminate\Database\QueryException $exception) {
+            return response()->json(['error' => 'Invalid Input'], 400);
         }
     }
 }
